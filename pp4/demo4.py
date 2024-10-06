c866ec0223aed3934d925d829bbd4815ea32a7a3
MariaVGit <enigmaticgirl0512@gmail.com>
2024-10-06 12:22:36 +0200

    Initial commit

---
+++ pp4/demo4.py
@@ -0,0 +1,20 @@
+print(14196%3 == 0)
+print(14196%4 == 0)
+print(14196%7  == 0)
+print(14196%13 == 0)
+
+print(14199%3 == 0)
+print(14199%4 == 0)
+print(14199%7 == 0)
+print(14199%13 ==0)
+
+print("Początkowa wartość inwestycji",14000.,"zł")
+print("Wartość inwestycji po pierwszym roku",14000. * 1.4, "zł")
+print("Wartość inwestycji po drugim roku", (14000.*1.4)-1500, "zł")
+print("Wartość inwestycji po trzecim roku")
+
+print(0o777)
+print(0b1011)
+print(0xFFF)
+
+
