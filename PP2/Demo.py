d1c9d659d91d1baa9c963c4d1451b3110ace26f2
MariaVGit <enigmaticgirl0512@gmail.com>
2025-03-16 12:08:07 +0100

    Commit

---
+++ PP2/Demo.py
@@ -0,0 +1,12 @@
+print("      *   "     )
+print("   **  **   " )
+print(" ****",      "  ****")
+print("    *",      " *")
+print("    *",      " *")
+
+print("Te trzy linie tekstu", "\n", "zostały wyświetlone", "\n", "za pomocą jednej instrukcji pythona")
+
+print("Będę uważał na zajęciach z Pythona \n" *100)
+
+
+
